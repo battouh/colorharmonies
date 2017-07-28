62c5e184e43c6796475e69a12f51d2335e4c9c96
Baptiste Manteau <baptiste.manteau@allegorithmic.com>
2017-07-28 12:15:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(name='colorharmonies',
       version='0.1',
-      description='Library to generate all your color harmonies with simplicity',
+      description='Library to generate all your color harmonies with simplicity!',
       url='https://github.com/baptistemanteau/colorharmonies',
       author='Baptiste Manteau',
       author_email='manteau.baptiste@gmail.com',
