8bd772eddc1c312e01bb957a2d2de91cd9272917
Baptiste Manteau <baptiste.manteau@allegorithmic.com>
2017-07-31 15:19:26 +0200

    patch 1.0.5

--- setup.py
+++ setup.py
@@ -2,8 +2,15 @@
 from setuptools import setup
 
 setup(name='colorharmonies',
-      version='1.0.4',
+      version='1.0.5',
       description='Library to generate all your color harmonies with simplicity!',
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion',
+      ],
+      keywords='color management RGB harmonies complementary monochromatic analogous',
       url='https://github.com/baptistemanteau/colorharmonies',
       author='Baptiste Manteau',
       author_email='manteau.baptiste@gmail.com',
